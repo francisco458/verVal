d23e23017d15d993a1ddcb49a675597576ef5301
francisco.jimenez <francisco.jimenez@DESW90P.Quipux.local>
2015-04-09 14:27:09 -0500

    sdfsd

---
+++ PracticaFinalVerVal/src/co/com/eafit/entidadbancaria/Cuenta.java
@@ -0,0 +1,14 @@
+package co.com.eafit.entidadbancaria;
+
+public class Cuenta {
+	public static void main(String[] args) {
+		// Prueba de cambios locales
+		// Prueba de cambios, luego push
+		System.out.println("HOLA MUNDO");
+		
+		// dfs
+		
+		// Pacho
+		// andres
+	}
+}
